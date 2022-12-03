8960543e3693830af7693ab4accb0ab025f244e2
Innovix <htetnainghein@innovix-solutions.com>
2022-12-03 19:54:13 +0630

    testing env is work in deployment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 const app = express();
-dotenv.config({path: './.env'});
+dotenv.config({ path: './.env' });
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -17,14 +17,14 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello to Backend API!');
-})  
+})
 
-mongoose.connect(process.env.CONNECTION_URL, {
+mongoose.connect('mongodb+srv://zeal:zeal123@project-dev-cluster.hqvcenr.mongodb.net/express_oauth?retryWrites=true&w=majority', {
   // useNewUrlParser: true, 
   // useUnifiedTopology: true 
 }).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-.catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect`));
