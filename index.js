69eb30e2b5c75f3bf72eeeb8d253555827dc7100
Htet Naing Hein <htetnainghein2001@gmail.com>
2024-02-15 09:21:47 +0630

    MONGO_URI

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello to Backend API!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello to Backend API!");
+});
 
-mongoose.connect('mongodb+srv://zeal:zeal123@project-dev-cluster.hqvcenr.mongodb.net/express_oauth?retryWrites=true&w=majority', {
-  // useNewUrlParser: true, 
-  // useUnifiedTopology: true 
-}).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(Process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
